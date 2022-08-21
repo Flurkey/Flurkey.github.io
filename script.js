e8e03492a52c2be5704bec6e4d6a74190e0bdac7
Waifuu <yoyo74456@gmail.com>
2022-08-21 11:20:41 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+var size = 5%;
+
+var divField = document.createElement("div");
+divField.style.width = size;
+divField.style.height = size;
+divField.style.background = "black";
+divField.style.cursor = "pointer"
+
+function crField() {
+  for (let i = 0; i > 100/size; i++) {
+    for (let i = 0; i > 100/size; i++) {
+      document.getElementById("main").appendChild(div);
+    }
+  }
+}
