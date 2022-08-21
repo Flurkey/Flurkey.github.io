33ea23466096a6513519a6b42b9c31d44d651c83
Waifuu <yoyo74456@gmail.com>
2022-08-21 12:02:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 var size = 5;
 
 function crField() {
-  for (let i = 0; i <= 100/size; i++) {
-    for (let i = 0; i <= 100/size; i++) {
+  for (let i = 0; i < 100/size; i++) {
+    for (let i = 0; i < 100/size; i++) {
       var divField = document.createElement("div");
       divField.style.width = size+"%";
       divField.style.height = size+"%";
