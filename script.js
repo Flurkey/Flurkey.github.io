d68bc63bece9f75fad6d948e1a8280a5a0a75706
Waifuu <yoyo74456@gmail.com>
2022-08-22 09:50:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -87,16 +87,18 @@ function fieldClick(btn) {
     if (btn.getAttribute("mine") == "false" && btn.getAttribute("clicked") == "false") {
       btn.setAttribute("clicked", true);
       showNum(btn)
-      var curLocation = btn.id.split(":");
-      for (let _ = -1; _ < 2; _++) {
-        for (let k = -1; k < 2; k++) {
-          var x = parseInt(curLocation[0])+_;
-          var y = parseInt(curLocation[1])+k;
-          if (document.getElementById(x+":"+y) != null) {
-            if(parseInt(document.getElementById(x+":"+y).getAttribute("near")) == 0) {
-              fieldClick(document.getElementById(x+":"+y));
-            } else {
-              showNum(document.getElementById(x+":"+y));
+      if (btn.getAttribute("near") == 0) {
+        var curLocation = btn.id.split(":");
+        for (let _ = -1; _ < 2; _++) {
+          for (let k = -1; k < 2; k++) {
+            var x = parseInt(curLocation[0])+_;
+            var y = parseInt(curLocation[1])+k;
+            if (document.getElementById(x+":"+y) != null) {
+              if(parseInt(document.getElementById(x+":"+y).getAttribute("near")) == 0) {
+                fieldClick(document.getElementById(x+":"+y));
+              } else {
+                showNum(document.getElementById(x+":"+y));
+              }
             }
           }
         }
